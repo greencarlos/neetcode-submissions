c3db1063d0f6e63a613c9dedd53eb5ca42f5600f
Money Making Mitch <verdeclos@gmail.com>
2026-08-02 12:25:48 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.toLowerCase().split('').filter(x => x.match(/[A-za-z0-9]/g)).join('')
+        let left = 0
+        let right = s.length - 1
+
+        while(left < right) {
+            if (s[left] !== s[right]) {
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
