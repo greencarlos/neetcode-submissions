a14e8f55414b31b84c218a318c0ceb47e551ce2a
Money Making Mitch <verdeclos@gmail.com>
2026-08-02 12:07:28 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const visited = {}
+
+        for (let i = 0; i < nums.length; i++) {
+            if (visited[target - nums[i]] > -1) return [i, visited[target - nums[i]]]
+
+            visited[nums[i]] = i
+        }
+        // return []
+    }
+}
