c487f6553ddc67264b78a8f6a3c4efcdf94958dd
Money Making Mitch <verdeclos@gmail.com>
2026-08-02 18:49:14 -0700

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+
+        const dfs = node => {
+            if (!node) return
+            console.log('node', node.val)
+//            [node.left, node.right] = [node.right, node.left]
+let tmp = node.left
+node.left = node.right
+node.right = tmp
+
+            dfs(node.left)
+            dfs(node.right)
+        }
+
+        dfs(root)
+        return root
+    }
+}
